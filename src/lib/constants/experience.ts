670cdad61830b48832a2c105b869fe5b8e95e3b4
brian_andela <brian_andela@medable.com>
2025-07-10 17:09:27 +0300

    feat(): add more details on my resume, play around with projects section

--- src/lib/constants/experience.ts
+++ src/lib/constants/experience.ts
@@ -2,40 +2,11 @@ import type { Experience } from "../types";
 
 export const EXPERIENCES: Experience[] = [
   {
-    company: "Medable",
-    positions: [
-      {
-        title: "Frontend Engineer (Contract)",
-        year: "05.2019 - present",
-        description: `
-- ♦ Developed the initial production web application for Site using React, Mobx, and Styled Components in collaboration with the product manager and another engineer. This initiative enabled doctors to conduct clinical trials on any desktop browser, resulting in cost reduction for Medable. This work contributed to Medable securing $91M in funding to further digital and decentralized clinical trials.
-- ♦ Helped lead the redesign and rebuild of Medable's new Site experience using Typescript, React, TailwindCSS with accessibility and performance in mind as part of the One, One and One decentralized clinical trial strategy, collaborating with a senior frontend engineer. This effort was instrumental in securing a $50M Flu study for Medable. User interface examples from this work are available in the product manager's recognition post and the CEO's announcement. Moreover, I developed the in-built training feature allowing easier understanding on how to use the Medable for sites experience.
-            `,
-        skills: [
-          "TypeScript",
-          "React",
-          "Javascript",
-          "Tailwind CSS",
-          "Node.js",
-          "Docker",
-          "React Testing Library",
-          "Playwright",
-          "Git",
-          "Gitlab",
-          "I18next",
-          "A11y",
-          "React Query",
-          "Storybook",
-        ]
-      },
-    ],
-  },
-  {
     company: "Andela",
     positions: [
       {
-        title: "Fullstack Engineer",
-        year: "07.2017 - Present",
+        title: "Software Engineer",
+        year: "07.2017 - Present, 8 years",
         description: `
 - ♦ Worked on multiple teams building various internal web solutions.
 - ♦ Projects: Andela Resource Tool, Andela Meals
@@ -56,30 +27,103 @@ export const EXPERIENCES: Experience[] = [
       },
     ],
   },
-//       {
-//         title: "Backend Engineer",
-//         year: "01.2024 - 04.2024",
-//         description: `
-// - ♦ Implemented **backend functionalities**, designed **database models**, and optimized APIs using **FastAPI, Python, and MongoDB**.
-// - ♦ Developed and maintained a **background worker system** using **ARQ and Redis**.
-// - ♦ Brainstormed and debugged **new features and functionalities** with the engineering team.
-// - ♦ Refactored and cleaned code, implementing **better coding patterns** and enforcing **formatting & linting rules** using **Ruff**.
-// - ♦ Deployed backend services using **Docker & Docker Compose** and improved **CI/CD pipelines** with **GitHub Actions**.
-//             `,
-//         skills: [
-//           "Python",
-//           "FastAPI",
-//           "MongoDB",
-//           "Docker",
-//           "Redis",
-//           "ARQ",
-//           "GoLang",
-//           "Next.js",
-//           "React",
-//         ],
-//       },
-//     ],
-//   },
+  {
+    company: "Medable",
+    positions: [
+      {
+        title: "Frontend Engineer (Contract)",
+        year: "05.2019 - 07.2025, 6 years",
+        description: `
+- ♦ Developed the initial production web application for Site using **React, Mobx, and Styled Components** in collaboration with the product manager and another engineer. This initiative enabled doctors to conduct clinical trials on any desktop browser, resulting in cost reduction for Medable. This work contributed to Medable secure funding to further digital and decentralized clinical trials.
+- ♦ Helped lead the redesign and rebuild of Medable's new Site experience using **Typescript, React, TailwindCSS** with accessibility and performance in mind as part of the One, One and One decentralized clinical trial strategy, collaborating with a senior frontend engineer. This effort was instrumental in securing a $50M Flu study for Medable. User interface examples from this work are available in [**the product manager's recognition post**](https://www.linkedin.com/posts/erikamotley_medable-for-sitesintuitive-seamless-reliable-activity-7336141432379887618-QLp4?utm_source=share&utm_medium=member_desktop&rcm=ACoAABQ7OM0BTjaDKtsOpttyciL-DbngQtgIqv4) and [**the CEO's announcement**](https://www.linkedin.com/posts/michellelongmire_teammedable-activity-7328082425832493056-kFid?utm_source=share&utm_medium=member_desktop&rcm=ACoAABQ7OM0BTjaDKtsOpttyciL-DbngQtgIqv4). Moreover, I developed the in-built training feature allowing easier understanding on how to use the Medable for sites experience.
+- ♦ Built a feature allowing Site users to view survey task response data and audit history entries increasing traceability and accountability in clinical trials on the web version of the Site application.           
+- ♦ Re-designed and implemented reference to language and region codes in the custom web framework with the aim of allowing standardisation **(using Intl.Locale as per ECMAscript standard)**, reducing code complexity allowing unit tests to be written and impacting customer value as more languages with scripts can be supported
+- ♦ More details on my resume [**here**](https://www.linkedin.com/posts/brian-hawi-odhiambo-95180a95_medable-is-now-a-unicorn-activity-7205101100200001536-_000?utm_source=share&utm_medium=member_desktop)
+
+`,
+skills: [
+          "TypeScript",
+          "React",
+          "Javascript",
+          "Tailwind CSS",
+          "Node.js",
+          "Docker",
+          "React Testing Library",
+          "Playwright",
+          "Git",
+          "Gitlab",
+          "I18next",
+          "A11y",
+          "React Query",
+          "Storybook",
+        ]
+      },
+    ],
+  },
+  {
+    company: "Daupler",
+    positions: [
+      {
+        title: "Frontend Developer",
+        year: "09.2018 - 02.2019, 6 months",
+        description: `
+- ♦ Worked on an internal tool integrating **GraphQL APIs** and **React Apollo Client** and **Material UI** that helped the sales and product team configure and manage the incident response management system for field crews and operations managers. Configuration of the incident response management system was reduced to a few hours instead of 2 or more days.
+            `,
+        skills: [
+          "React",
+          "GraphQL",
+          "Apollo Client",
+          "Material UI",
+          "Node.js",
+          "Git",
+        ],
+      },
+    ],
+  },
+  {
+    company: "Kilimall International",
+    positions: [
+      {
+        title: "Software Developer",
+        year: "11.2015 - 12.2016, 1 year",
+        description: `
+- ♦ Enhanced UI/UX for key Kilimall campaigns. Integrated the new M-Pesa API to expand customer reach. Created a [**user-friendly guide for integrating with Lipa Na M-Pesa**](https://github.com/HawiCaesar/pay_confirmation).
+            `,
+        skills: [
+          "Javascript",
+          "PHP",
+          "JQuery",
+          "HTML",
+          "CSS",
+          "Git",
+          "Github",
+        ],
+      },
+    ],
+  },
+  {
+    company: "HP Strathmore Research Lab",
+    positions: [
+      {
+        title: "Web Developer",
+        year: "11.2013 - 10.2015, 1 year 10 months",
+        description: `
+- ♦ Built web-based health solutions for the Ministry of Health in Kenya allowing better reporting, analytics and ultimately better resource allocation to remote hospitals in Kenya. Achieved this with **PHP - CodeIgniter**.
+- ♦ More details of the work we did here [**here**](https://www.youtube.com/watch?v=VKxUVXX_YWI)
+
+`,
+        skills: [
+          "Javascript",
+          "PHP, CodeIgniter",
+          "JQuery",
+          "HTML",
+          "CSS",
+          "Git",
+          "Github",
+        ],
+      },
+    ],
+  }
 
 //   {
 //     company: "Freelance | Upwork | Locally",
--- src/lib/constants/profile.ts
+++ src/lib/constants/profile.ts
@@ -1,11 +1,11 @@
 export const PROFILE_INFO = {
   role: 'Software Engineer / Frontend Developer',
   logo: 'HawiCaesar',
-  slogan: 'Coding, Creativity, Coffee...',
+  slogan: 'Let\'s build the future together',
   displayName: "Brian Hawi Odhiambo",
   email: "brianhawi92@gmail.com",
   about: `
-I'm a full stack **software engineer** with 7 years of experience specializing in **frontend web development**. A **custodian for frontend experiences** with the ability to develop and maintain high-quality, resilient, accessible, and performant web applications. In addition to frontend development, I have experience in test-driven development (TDD) and backend development. Possesses 6 years of remote work experience
+I'm a full stack **software engineer** with 7 years of experience specializing in **frontend web development**. A **custodian for frontend experiences** with the ability to develop and maintain high-quality, resilient, accessible, and performant web applications. In addition to frontend development, I have experience in test-driven development (TDD) and backend development. I have 6 years of remote work experience
 `,
 } as const;
 
